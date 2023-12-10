eb1ad387180a4c1ec6f222d2cc3ed8f9558c9101
Roman Zuykov <rom4n364@gmail.com>
2023-12-10 21:04:58 +0300

    add setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+from os.path import dirname, abspath
+
+from setuptools import setup
+
+
+requirements_file = dirname(abspath(__file__)) + "/requirements.txt"
+with open(requirements_file, "r") as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name="calculator-bot",
+    version="2.0.0",
+    packages=["calculator_bot"],
+    install_requires=requirements,
+    entry_points={
+        "console_scripts": [
+            "calculator-bot = calculator_bot.app:main",
+        ],
+    },
+    include_package_data=True,
+)
